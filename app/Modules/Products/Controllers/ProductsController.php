3be35c48ba55c7acb2e6c5e91cdb1d59d07cb178
MohanManirul <fiforeg@gmail.com>
2024-12-12 14:49:43 +0600

    create migration file to specific module

---
+++ app/Modules/Products/Controllers/ProductsController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Modules\Products\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProductsController extends Controller
+{
+    //
+}
